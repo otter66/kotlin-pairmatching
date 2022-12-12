65b719117ef8e39d14a6e70236348699471631a2
otter <a33397397@gmail.com>
2022-12-12 20:56:16 +0900

    feat(view): 입력에 유효성 검사 적용

--- kotlin-pairmatching/src/main/kotlin/pairmatching/view/InputView.kt
+++ kotlin-pairmatching/src/main/kotlin/pairmatching/view/InputView.kt
@@ -1,18 +1,25 @@
 package pairmatching.view
 
+import pairmatching.domain.Validator
 import pairmatching.values.REMATCH_MESSAGE
 
-class InputView {
+class InputView(
+    private val validator: Validator
+) {
 
-    fun  readFunctionOption(): String {
-        val input = readln()
-
-        return input
+    fun readFunctionOption(): String {
+        try {
+            val input = readln()
+            validator.inspectFunctionOption(input)
+            return input
+        } catch (e: IllegalArgumentException) {
+            throw e
+        }
     }
 
     fun readRematchOption(): Boolean {
         val input = readln()
-
+        validator.inspectRematchOption(input)
         return input == REMATCH_MESSAGE
     }
 
